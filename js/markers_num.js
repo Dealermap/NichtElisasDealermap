fefc10d2cad67c2b6f901c5758608ac7804599e5
flowster543 <86293019+flowster543@users.noreply.github.com>
2021-10-26 20:12:35 +0200

    Update markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -34,3 +34,9 @@ L.marker([-7628.5, 4501.75], {icon: cardIcon6}).addTo(map)
   .bindPopup("<h2>Hafen 4</h3><p><a href='https://i.imgur.com/NZH0Mcn.jpg' data-lightbox='image-1' data-title='Hafen 4'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-7108, 3836.5], {icon: cardIcon6}).addTo(map)
   .bindPopup("<h2>Hafen 19</h3><p><a href='https://i.imgur.com/TwXjkog.jpg' data-lightbox='image-1' data-title='Hafen 19'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-6570.25, 4294.25], {icon: cardIcon2}).addTo(map)
+  .bindPopup("<h2>Cypress Flats 9</h3><p><a href='https://i.imgur.com/9AviCey.jpg' data-lightbox='image-1' data-title='Cypress Flats 9'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-7341.75, 2574], {icon: cardIcon9}).addTo(map)
+  .bindPopup("<h2>Airport 4</h3><p><a href='https://i.imgur.com/hP9mT68.png' data-lightbox='image-1' data-title='Airport 4'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-3468.5, 3862.25], {icon: cardIcon4}).addTo(map)
+  .bindPopup("<h2>Harmony 3</h3><p><a href='https://i.imgur.com/LL17ROS.png' data-lightbox='image-1' data-title='Harmony 3'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
