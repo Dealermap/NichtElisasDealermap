0dbddb4118dde4c717a9072c7b48c73bc1f8c727
flowster543 <86293019+flowster543@users.noreply.github.com>
2021-10-27 13:38:06 +0200

    Update markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -58,3 +58,7 @@ L.marker([-7600, 6410], {icon: cardIcon7}).addTo(map)
   .bindPopup("<h2>Cayo Perico 16</h3><p><a href='https://i.imgur.com/9CaEI27.jpg' data-lightbox='image-1' data-title='Cayo Perico 16'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-5314.5, 4548.5], {icon: cardIcon8}).addTo(map)
   .bindPopup("<h2>Rennbahn 3</h3><p><a href='https://i.imgur.com/qgOov0H.jpg' data-lightbox='image-1' data-title='Rennbahn 3'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-6708, 3929.75], {icon: cardIcon9}).addTo(map)
+  .bindPopup("<h2>Rancho 2</h3><p><a href='https://i.imgur.com/EaAUZws.jpg' data-lightbox='image-1' data-title='Rancho 2'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-6760, 4058.75], {icon: cardIcon9}).addTo(map)
+  .bindPopup("<h2>Rancho 6</h3><p><a href='https://i.imgur.com/KBxmosq.jpg' data-lightbox='image-1' data-title='Rancho 6'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
