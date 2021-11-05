3d0370f20f44e511391cf0fb29b985e4ecaab577
flowster543 <86293019+flowster543@users.noreply.github.com>
2021-11-05 02:24:06 +0100

    Update markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -142,7 +142,7 @@ L.marker([-2415, 4899], {icon: cardIcon4}).addTo(map)
   .bindPopup("<h2>Grapeseed 3</h3><p><a href='https://i.imgur.com/hdxNisj.png' data-lightbox='image-1' data-title='Grapeseed 3'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-6888, 4028], {icon: cardIcon9}).addTo(map)
   .bindPopup("<h2>Rancho 3</h3><p><a href='https://i.imgur.com/vTU6hn7.jpg' data-lightbox='image-1' data-title='Rancho 3'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
-L.marker([-6350, 3022], {icon: cardIcon2}).addTo(map)
+L.marker([-6350, 3022], {icon: cardIcon3}).addTo(map)
   .bindPopup("<h2>Vespucci Kanaele 2</h3><p><a href='https://i.imgur.com/WpXJLY6.jpg' data-lightbox='image-1' data-title='Vespucci Kanaele 2'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-7006, 4819], {icon: cardIcon2}).addTo(map)
   .bindPopup("<h2>Cypress Flats 5</h3><p><a href='https://i.imgur.com/5ulryOy.png' data-lightbox='image-1' data-title='Cypress Flats 5'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
