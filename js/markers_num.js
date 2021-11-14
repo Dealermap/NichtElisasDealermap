2cf0a55604235079809e2e01e87e3b77616f3835
flowster543 <86293019+flowster543@users.noreply.github.com>
2021-11-14 02:46:13 +0100

    Update markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -260,3 +260,5 @@ L.marker([-5265.5, 3140.5], {icon: cardIcon3}).addTo(map)
   .bindPopup("<h2>Vinewood 10</h3><p><a href='https://i.imgur.com/zmyilWM.png' data-lightbox='image-1' data-title='Vinewood 10'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-7291, 3778], {icon: cardIcon6}).addTo(map)
   .bindPopup("<h2>Hafen 20</h3><p><a href='https://i.imgur.com/OdH2vU2.jpg' data-lightbox='image-1' data-title='Hafen 20'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-3627, 3956], {icon: cardIcon4}).addTo(map)
+  .bindPopup("<h2>Harmony 2</h3><p><a href='https://i.imgur.com/a3ElbJK.jpg' data-lightbox='image-1' data-title='Harmony 2'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
