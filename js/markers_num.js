4e20849834856d35b7d6f3900bb77ef75958f86f
Dealermap <162360988+Dealermap@users.noreply.github.com>
2024-04-09 17:23:38 +0200

    Update markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -276,3 +276,5 @@ L.marker([-7535, 2469.75], {icon: cardIcon9}).addTo(map)
   .bindPopup("<h2>Airport 3</h3><p><a href='https://i.imgur.com/29ee7sy.png' data-lightbox='image-1' data-title='Airport 3'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-7217, 3021], {icon: cardIcon9}).addTo(map)
   .bindPopup("<h2>Airport 6</h3><p><a href='https://i.imgur.com/ibuvVbx.jpeg' data-lightbox='image-1' data-title='Airport 6'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-6894.5, 2870.75], {icon: cardIcon9}).addTo(map)
+  .bindPopup("<h2>Airport 9</h3><p><a href='https://i.imgur.com/CrDISbW.jpeg' data-lightbox='image-1' data-title='Airport 9'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
