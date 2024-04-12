1e311965372a601c89d24184d94dea3766df8a20
Dealermap <162360988+Dealermap@users.noreply.github.com>
2024-04-13 00:23:49 +0200

    markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -14,6 +14,8 @@ L.marker([-7552.25, 4617.5], {icon: cardIcon6}).addTo(map)
   .bindPopup("<h2>Hafen 5</h3><p><a href='https://i.imgur.com/imrLspb.jpeg' data-lightbox='image-1' data-title='Hafen 5'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-7499.5, 4113], {icon: cardIcon6}).addTo(map)
   .bindPopup("<h2>Hafen 9</h3><p><a href='https://i.imgur.com/VIhuB8t.jpeg' data-lightbox='image-1' data-title='Hafen 9'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-7550.75, 4061], {icon: cardIcon6}).addTo(map)
+  .bindPopup("<h2>Hafen 11</h3><p><a href='https://i.imgur.com/t0J3FQH.jpeg' data-lightbox='image-1' data-title='Hafen 11'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-7610, 6410], {icon: cardIcon7}).addTo(map)
   .bindPopup("<h2>CayoPerico 17</h3><p><a href='https://i.imgur.com/JOklVot.png' data-lightbox='image-1' data-title='CayoPerico 17'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-7620, 6420], {icon: cardIcon7}).addTo(map)
