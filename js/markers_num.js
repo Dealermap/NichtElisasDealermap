18d19b918906c36ef4aba5a0da0f69d7b6b2afe8
Dealermap <162360988+Dealermap@users.noreply.github.com>
2024-04-15 12:02:59 +0200

    markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -386,3 +386,5 @@ L.marker([-3316.5, 4790.5], {icon: cardIcon4}).addTo(map)
   .bindPopup("<h2>Sandy Shores 7</h3><p><a href='https://i.imgur.com/qVb0GvN.png' data-lightbox='image-1' data-title='Sandy Shores 7'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-6419, 4021], {icon: cardIcon9}).addTo(map)
   .bindPopup("<h2>Strawberry 1</h3><p><a href='https://i.imgur.com/2EVylep.jpeg' data-lightbox='image-1' data-title='Strawberry 1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-4300.5, 2215.5], {icon: cardIcon4}).addTo(map)
+  .bindPopup("<h2>Tongva Hills 1</h3><p><a href='https://i.imgur.com/XAr24nv.jpeg' data-lightbox='image-1' data-title='Tongva Hills 1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
