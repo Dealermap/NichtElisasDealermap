95e476337f158c38ab07a64d56085cd846402b8d
Dealermap <162360988+Dealermap@users.noreply.github.com>
2024-04-15 11:42:27 +0200

    markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -382,3 +382,5 @@ L.marker([-3105.5, 4792.5], {icon: cardIcon4}).addTo(map)
   .bindPopup("<h2>Sandy Shores 4</h3><p><a href='https://i.imgur.com/6jQNbDs.jpeg' data-lightbox='image-1' data-title='Sandy Shores 4'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-2951, 5451], {icon: cardIcon4}).addTo(map)
   .bindPopup("<h2>Sandy Shores 5</h3><p><a href='https://i.imgur.com/03ql8xg.jpeg' data-lightbox='image-1' data-title='Sandy Shores 5'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-3316.5, 4790.5], {icon: cardIcon4}).addTo(map)
+  .bindPopup("<h2>Sandy Shores 7</h3><p><a href='https://i.imgur.com/qVb0GvN.png' data-lightbox='image-1' data-title='Sandy Shores 7'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
