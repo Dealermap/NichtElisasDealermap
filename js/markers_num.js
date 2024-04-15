2998f6ed919e1c2d9117218ed6985318a7abc482
Dealermap <162360988+Dealermap@users.noreply.github.com>
2024-04-15 12:14:05 +0200

    markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -394,3 +394,5 @@ L.marker([-4993.5, 3812], {icon: cardIcon3}).addTo(map)
   .bindPopup("<h2>Vinewood 4</h3><p><a href='https://i.imgur.com/ADuSeRC.jpeg' data-lightbox='image-1' data-title='Vinewood 4'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-5184, 4231.5], {icon: cardIcon3}).addTo(map)
   .bindPopup("<h2>Vinewood 7</h3><p><a href='https://i.imgur.com/JM9eVC0.jpeg' data-lightbox='image-1' data-title='Vinewood 7'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-5441, 2861.75], {icon: cardIcon3}).addTo(map)
+  .bindPopup("<h2>Vinewood 14</h3><p><a href='https://i.imgur.com/JRxHWFt.png' data-lightbox='image-1' data-title='Vinewood 14'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
