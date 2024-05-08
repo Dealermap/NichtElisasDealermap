b91d608275432af697c02dbc0b8f36ae0533b28f
Dealermap <162360988+Dealermap@users.noreply.github.com>
2024-05-08 10:26:06 +0200

    markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -430,3 +430,5 @@ L.marker([-4964, 5515], {icon: cardIcon8}).addTo(map)
   .bindPopup("<h2>Tataviam Bergkette 3</h3><p><a href='https://i.imgur.com/q0mEhSp.jpeg' data-lightbox='image-1' data-title='Tataviam Bergkette 3'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-3674, 5623], {icon: cardIcon13}).addTo(map)
   .bindPopup("<h2>Davis Quartz 1</h3><p><a href='https://i.imgur.com/aMb2cIT.jpeg' data-lightbox='image-1' data-title='Davis Quartz 1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-7139, 3840], {icon: cardIcon55}).addTo(map)
+  .bindPopup("<h2>Hacking-Kit-Mulle 1</h3><p><a href='https://i.imgur.com/y4Fr2c8.jpeg' data-lightbox='image-1' data-title='Hacking-Kit-Mulle1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
