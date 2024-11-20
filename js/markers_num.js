ddc57a04f3ece00eca303d94b834f17bd44a2841
Dealermap <162360988+Dealermap@users.noreply.github.com>
2024-11-20 11:57:21 +0100

    markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -450,8 +450,8 @@ L.marker([-7639.5, 4223.5], {icon: cardIcon56}).addTo(map)
   .bindPopup("<h2>Hacking-Kit-Mulle 4</h3><p><a href='https://i.imgur.com/DL1Oz4e.jpeg' data-lightbox='image-1' data-title='Hacking-Kit-Mulle 4'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-7027.5, 3459.75], {icon: cardIcon56}).addTo(map)
   .bindPopup("<h2>Hacking-Kit-Mulle 5</h3><p><a href='https://i.imgur.com/G6e0y9v.jpeg' data-lightbox='image-1' data-title='Hacking-Kit-Mulle 5'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
-L.marker([-7298.5, 4862], {icon: cardIcon57}).addTo(map)
-L.marker([-7243.5, 4708], {icon: cardIcon57}).addTo(map)
-L.marker([-6989, 3891], {icon: cardIcon57}).addTo(map)
-L.marker([-6888, 3675], {icon: cardIcon57}).addTo(map)
+L.marker([-1201.375, 3606.000], {icon: cardIcon57}).addTo(map)
+  .bindPopup("<h2>Keycard 1</h3><p><a href='https://i.imgur.com/2oxwyWW.png' data-lightbox='image-1' data-title='Keycard 1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+
+
 
