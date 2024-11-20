509e3cf55246f30ff607535f87b5f51b48bcdff4
Dealermap <162360988+Dealermap@users.noreply.github.com>
2024-11-20 11:29:06 +0100

    markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -450,3 +450,5 @@ L.marker([-7639.5, 4223.5], {icon: cardIcon56}).addTo(map)
   .bindPopup("<h2>Hacking-Kit-Mulle 4</h3><p><a href='https://i.imgur.com/DL1Oz4e.jpeg' data-lightbox='image-1' data-title='Hacking-Kit-Mulle 4'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-7027.5, 3459.75], {icon: cardIcon56}).addTo(map)
   .bindPopup("<h2>Hacking-Kit-Mulle 5</h3><p><a href='https://i.imgur.com/G6e0y9v.jpeg' data-lightbox='image-1' data-title='Hacking-Kit-Mulle 5'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-7298.5, 4862], {icon: cardIcon57}).addTo(map)
+
