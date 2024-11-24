809724796fc0e461d85e26679a9c435dbd7b704a
Dealermap <162360988+Dealermap@users.noreply.github.com>
2024-11-24 07:58:59 +0100

    markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -64,8 +64,8 @@ L.marker([-5565.5, 4847], {icon: cardIcon8}).addTo(map)
   .bindPopup("<h2>Staudamm 1</h3><p><a href='https://i.imgur.com/pvZ7XTM.png' data-lightbox='image-1' data-title='Staudamm 1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-2321, 3391], {icon: cardIcon1}).addTo(map)
   .bindPopup("<h2>Saegewerk 2</h3><p><a href='https://i.imgur.com/9tXF7cg.png' data-lightbox='image-1' data-title='Saegewerk 2'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
-L.marker([-1967, 3393], {icon: cardIcon1}).addTo(map)
-  .bindPopup("<h2>Saegewerk 3</h3><p><a href='https://i.imgur.com/NCVm4GG.jpg' data-lightbox='image-1' data-title='Saegewerk 3'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-2136.5, 3193.5], {icon: cardIcon1}).addTo(map)
+  .bindPopup("<h2>Saegewerk 3</h3><p><a href='https://i.imgur.com/Z5cNSRR.png' data-lightbox='image-1' data-title='Saegewerk 3'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-7590, 6390], {icon: cardIcon7}).addTo(map)
   .bindPopup("<h2>Cayo Perico 2</h3><p><a href='https://i.imgur.com/gUdHv8h.jpg' data-lightbox='image-1' data-title='Cayo Perico 2'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-5854, 4416], {icon: cardIcon8}).addTo(map)
