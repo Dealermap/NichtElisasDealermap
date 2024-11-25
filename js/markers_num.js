3cfe9e5ec5afeb94fa170b98a0d02b531235cec1
Dealermap <162360988+Dealermap@users.noreply.github.com>
2024-11-25 18:06:43 +0100

    markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -428,7 +428,7 @@ L.marker([-5741.25, 2427.25], {icon: cardIcon5}).addTo(map)
   .bindPopup("<h2>West Highway 9</h3><p><a href='https://i.imgur.com/VHkwxgT.png' data-lightbox='image-1' data-title='West Highway 9'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-4248, 5163], {icon: cardIcon8}).addTo(map)
   .bindPopup("<h2>Windpark 1</h3><p><a href='https://i.imgur.com/LPuOY81.png' data-lightbox='image-1' data-title='Windpark 1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
-L.marker([-4056.5, 5163], {icon: cardIcon8}).addTo(map)
+L.marker([-4058.25, 5307.25], {icon: cardIcon8}).addTo(map)
   .bindPopup("<h2>Windpark 2</h3><p><a href='https://i.imgur.com/OiM9O5G.png' data-lightbox='image-1' data-title='Windpark 2'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-4685.25, 5306], {icon: cardIcon8}).addTo(map)
   .bindPopup("<h2>Windpark 3</h3><p><a href='https://i.imgur.com/D995wMZ.jpeg' data-lightbox='image-1' data-title='Windpark 3'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
