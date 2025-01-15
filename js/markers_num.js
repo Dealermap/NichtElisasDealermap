d8f23790e3c4ea45e4e2fb2d741a8ada7b958bd4
Dealermap <162360988+Dealermap@users.noreply.github.com>
2025-01-15 08:43:17 +0100

    markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -492,6 +492,8 @@ L.marker([-3559, 5749.5], {icon: cardIcon13}).addTo(map)
   .bindPopup("<h2>Davis Quartz 3</h3><p><a href='https://i.imgur.com/FQee0Rf.jpeg' data-lightbox='image-1' data-title='Davis Quartz 3'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-3632.25, 5748.75], {icon: cardIcon13}).addTo(map)
   .bindPopup("<h2>Davis Quartz 2</h3><p><a href='https://i.imgur.com/Vfo1H6Y.jpeg' data-lightbox='image-1' data-title='Davis Quartz 2'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-3226, 5711], {icon: cardIcon13}).addTo(map)
+  .bindPopup("<h2>San Chianski Bergkette</h3><p><a href='https://i.imgur.com/9w7IVqD.png' data-lightbox='image-1' data-title='San Chianski Bergkette'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-5926, 3476.75], {icon: cardIcon5}).addTo(map)
   .bindPopup("<h2>Little Seoul 2</h3><p><a href='https://i.imgur.com/odUL5fC.jpeg' data-lightbox='image-1' data-title='Little Seoul 2'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-4964, 5515], {icon: cardIcon8}).addTo(map)
